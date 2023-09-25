5265f5478b378801eb0fce03d5c8f0e51dc866c1
Tanu Chouhan <tanuchouhan161@gmail.com>
2023-09-26 00:58:08 +0530

    program to find Frequency

--- src/datastructure/array/Frequency.java
+++ src/datastructure/array/Frequency.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Frequency {
     public static void main(String[] args) {
 
-        int arr[]={1,2,2,6,7,0,7,5,8,9,6,5,78,9,65,4,57,8,654,45,678,7,98,65,7,8,65,4,7,8,56,5};
+        int[] arr={1,2,2,6,7,0,7,5,8,9,6,5,78,9,65,4,57,8,654,45,678,7,98,65,7,8,65,4,7,8,56,5};
         ArrayList<Integer>unique=new ArrayList<>();
         for (int e :arr) {
             if (!unique.contains(e))
